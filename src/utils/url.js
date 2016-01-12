1bb3bbb8309170d933b8f46b9b786d97c1a62e26
bmacheski <bwm0007@gmail.com>
2016-01-12 15:59:14 -0600

    Add results view count to url util

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,23 +1,25 @@
 import * as types from '../constants'
 
+const view_count = '12'
+
 export function setUrl(category) {
   if (category === 'mostPopular') {
-   return `${types.BASE_URL}videos?part=statistics,snippet,contentDetails&maxResults=9&chart=${category}&key=${types.API_KEY}`
+   return `${types.BASE_URL}videos?part=statistics,snippet,contentDetails&maxResults=${view_count}&chart=${category}&key=${types.API_KEY}`
   }
   else {
-    return `${types.BASE_URL}videos?part=statistics,snippet,contentDetails&chart=mostPopular&maxResults=9&videoCategoryId=${category}&key=${types.API_KEY}`
+    return `${types.BASE_URL}videos?part=statistics,snippet,contentDetails&chart=mostPopular&maxResults=${view_count}&videoCategoryId=${category}&key=${types.API_KEY}`
   }
 }
 
 export function setNextUrl(category, pageToken) {
   if (category === 'mostPopular') {
-   return `${types.BASE_URL}videos?part=statistics,snippet,contentDetails&maxResults=9&chart=${category}&key=${types.API_KEY}&pageToken=${pageToken}`
+   return `${types.BASE_URL}videos?part=statistics,snippet,contentDetails&maxResults=${view_count}&chart=${category}&key=${types.API_KEY}&pageToken=${pageToken}`
   }
   else {
-    return `${types.BASE_URL}videos?part=statistics,snippet,contentDetails&chart=mostPopular&maxResults=9&videoCategoryId=${category}&key=${types.API_KEY}&pageToken=${pageToken}`
+    return `${types.BASE_URL}videos?part=statistics,snippet,contentDetails&chart=mostPopular&maxResults=${view_count}&videoCategoryId=${category}&key=${types.API_KEY}&pageToken=${pageToken}`
   }
 }
 
 export function setSearchUrl(query) {
- return `${types.BASE_URL}search?part=snippet,id&type=video&maxResults=12&q=${query}&key=${types.API_KEY}`
+ return `${types.BASE_URL}search?part=snippet,id&type=video&maxResults=${view_count}&q=${query}&key=${types.API_KEY}`
 }
